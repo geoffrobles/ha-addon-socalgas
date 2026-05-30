70d691fcd577c03f04a88fd81113bb99839a4811
geoffrobles <geoffrobles@gmail.com>
2026-05-30 14:45:43 -0700

    Debugging

--- socalgas/run.sh
+++ socalgas/run.sh
@@ -1,9 +1,20 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bashio
 
+export SOCALGAS_EMAIL=$(bashio::config 'email')
+export SOCALGAS_PASSWORD=$(bashio::config 'password')
+
+export MQTT_HOST=$(bashio::config 'mqtt_host')
+export MQTT_PORT=$(bashio::config 'mqtt_port')
+export MQTT_USER=$(bashio::config 'mqtt_user')
+export MQTT_PASSWORD=$(bashio::config 'mqtt_password')
 
 while true
 do
     echo "Running SoCalGas sync..."
+
+    echo "MQTT_HOST=$MQTT_HOST"
+    echo "EMAIL=$SOCALGAS_EMAIL"
+    echo "MQTT_USER=$MQTT_USER"
 
     python3 /app/socalgas_api_slim.py
 

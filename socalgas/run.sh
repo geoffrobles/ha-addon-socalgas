b446d88341f8ebd21d81519151791584d2062ee6
geoffrobles <geoffrobles@gmail.com>
2026-05-30 13:55:58 -0700

    More files

--- socalgas/run.sh
+++ socalgas/run.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/bash
 
-export SOCALGAS_EMAIL=$(bashio::config 'email')
-export SOCALGAS_PASSWORD=$(bashio::config 'password')
+while true
+do
+    echo "Running SoCalGas sync..."
 
-export MQTT_HOST=$(bashio::config 'mqtt_host')
-export MQTT_PORT=$(bashio::config 'mqtt_port')
-export MQTT_USER=$(bashio::config 'mqtt_user')
-export MQTT_PASSWORD=$(bashio::config 'mqtt_password')
+    python3 /app/socalgas_api_slim.py
 
-python3 /app/socalgas_api_slim.py
+    echo "Sleeping 6 hours..."
+
+    sleep 21600
+done
